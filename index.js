4bbaa4fb017704c453abfc7f561d2156b0486050
Shiny <contact@shinychang.net>
2020-04-17 00:54:29 +0800

    Only parse body when have

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ http
       data.push(chunk);
     });
     req.on('end', () => {
-      const json = JSON.parse(data);
-      const {
-        ref,
-        after,
-        repository: { full_name }
-      } = json;
-      const branch = ref.replace('refs/heads/', '');
-      if (branch === process.env.BRANCH_NAME) {
-        notifySlack({repo: full_name, branch, commit: after})
+      if (data.length) {
+        const json = JSON.parse(data.join(''));
+        const {
+          ref,
+          after,
+          repository: { full_name }
+        } = json;
+        const branch = ref.replace('refs/heads/', '');
+        if (branch === process.env.BRANCH_NAME) {
+          notifySlack({ repo: full_name, branch, commit: after });
+        }
       }
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write('OK!');
@@ -26,15 +28,15 @@ http
   })
   .listen(process.env.PORT || 8080);
 
-
-const notifySlack = ({commit, repo, branch}) => fetch(process.env.SLACK_WEBHOOK_URL, {
-  method: 'POST',
-  headers: {
-    'content-type': 'application/x-www-form-urlencoded'
-  },
-  body: JSON.stringify({
-    channel: process.env.SLACK_WEBHOOK_CHANNEL,
-    username: `bot`,
-    text: `${repo}/${branch}#${commit}`,
-  })
-})
+const notifySlack = ({ commit, repo, branch }) =>
+  fetch(process.env.SLACK_WEBHOOK_URL, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: JSON.stringify({
+      channel: process.env.SLACK_WEBHOOK_CHANNEL,
+      username: `bot`,
+      text: `${repo}/${branch}#${commit}`
+    })
+  });
