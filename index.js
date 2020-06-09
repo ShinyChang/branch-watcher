c76317ac17407cc87a3f5e531b40e8ddee2f7b06
Shiny <contact@shinychang.net>
2020-06-09 21:33:42 +0800

    Change format

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-require('dotenv').config();
-const http = require('http');
-const fetch = require('node-fetch');
+require("dotenv").config();
+const http = require("http");
+const fetch = require("node-fetch");
 
 http
   .createServer((req, res) => {
     let data = [];
-    req.on('data', chunk => {
+    req.on("data", (chunk) => {
       data.push(chunk);
     });
-    req.on('end', () => {
-      if (req.headers['x-github-event'] === 'push') {
-        const json = JSON.parse(data.join(''));
+    req.on("end", () => {
+      if (req.headers["x-github-event"] === "push") {
+        const json = JSON.parse(data.join(""));
         const {
           ref,
-          after,
-          repository: { full_name }
+          head_commit,
+          repository: { full_name },
         } = json;
-        const branch = ref.replace('refs/heads/', '');
+        const branch = ref.replace("refs/heads/", "");
         if (branch === process.env.BRANCH_NAME) {
-          notifySlack({ repo: full_name, branch, commit: after });
+          notifySlack({ repo: full_name, branch, commit: head_commit });
         }
       }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('OK!');
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("OK!");
       res.end();
     });
   })
@@ -30,13 +30,42 @@ http
 
 const notifySlack = ({ commit, repo, branch }) =>
   fetch(process.env.SLACK_WEBHOOK_URL, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'content-type': 'application/x-www-form-urlencoded'
+      "content-type": "application/x-www-form-urlencoded",
     },
     body: JSON.stringify({
       channel: process.env.SLACK_WEBHOOK_CHANNEL,
       username: `bot`,
-      text: `${repo}/${branch}#${commit}`
-    })
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `<${commit.url}|${commit.message}>`,
+          },
+        },
+        {
+          type: "section",
+          fields: [
+            {
+              type: "mrkdwn",
+              text: `*Repo:*\n${repo}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*Branch:*\n${branch}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*Hash:*\n${commit.id}`,
+            },
+            {
+              type: "mrkdwn",
+              text: `*Committer:*\n${commit.committer.name}`,
+            },
+          ],
+        },
+      ],
+    }),
   });
